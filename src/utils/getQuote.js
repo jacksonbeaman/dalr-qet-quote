5dc9ca7dea4b5a42ee5717848cf5aebdc6b66f74
jacksonbeaman <jacksonbeaman@gmail.com>
2022-07-28 17:15:52 -0700

    project init

---
+++ src/utils/getQuote.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const getQuote = async (symbol, iexToken) => {
+  try {
+    const settings = {
+      url: `/${symbol}/quote?token=${iexToken}`,
+      baseURL: `https://sandbox.iexapis.com/stable/stock`,
+      method: 'get',
+      timeout: 0,
+    };
+
+    const { data } = await axios(settings);
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// below syntax generates error: getQuote is not a function when using postman and serverless
+// module.exports = { getQuote };
+
+// below syntax works with postman and servless, but unrecongnized in getQuote.test.js
+// module.exports = getQuote;
+
+// below syntax must be destructered when imported for use in lambda - e.g.  { getQuote}
+exports.getQuote = getQuote;
+
+// exports = { getQuote };
